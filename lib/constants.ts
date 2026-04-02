0a81260a58390833ae51fff6d1a0ca2a3c9f2b20
SatanSanta17 <burhanuddinchital25151@gmail.com>
2026-04-03 01:42:17 +0530

    updated the email restriction

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,4 @@
  * Read from ALLOWED_EMAIL_DOMAIN env var. Must be set in production.
  */
 export const ALLOWED_EMAIL_DOMAIN =
-  process.env.ALLOWED_EMAIL_DOMAIN ?? "inmobi.com";
+  process.env.ALLOWED_EMAIL_DOMAIN ?? "gmail.com";
